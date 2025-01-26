9e72892dc6a984f00fecc855de1ae6e3396f6fe0
Joe Paci <125299952+pacijc@users.noreply.github.com>
2025-01-26 18:32:50 -0500

    added command line variables

--- user_code/connect.sh
+++ user_code/connect.sh
@@ -1,9 +1,9 @@
-to_add="/Users/joepaci/Downloads/Mosaic_Template"
-active="Mosaic_Template"
+to_add=$1
+active=$(basename "$to_add")
 base="~/PiSlideshow"
 set_active_line="rm -r $base/active_slideshow; cp -r $base/stored_slideshows/$active $base/active_slideshow;"
 
 #send a file to the pi, then reboot
 scp -r $to_add user@10.0.0.153:PiSlideshow/stored_slideshows/
 
-ssh user@10.0.0.153 "$set_active_line"
+ssh user@10.0.0.153 "$set_active_line sudo reboot"
