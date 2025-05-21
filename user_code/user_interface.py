5d05d379de877891d2b868daa146e644d4feff1a
Joe Paci <125299952+pacijc@users.noreply.github.com>
2025-05-21 12:15:55 -0400

    added text back and reformatted UI

--- user_code/user_interface.py
+++ user_code/user_interface.py
@@ -1,62 +1,66 @@
 from tkinter import *
 from tkinter import filedialog, messagebox, ttk
 from pdf2image import convert_from_path
-import subprocess
 import os
 
 DEFAULT_PAD = 10
 
+info_text = """HOW TO USE THIS APP
+
+1. Click 'Browse Files' and select the PDF containing your slideshow
+2. Once your file has been selected, press 'Convert' to prepare the slides
+3. Click 'Quit' to exit program
+4. Connect device to tv in order to start slideshow
+
+To change slide duration, update value in the slider before pressing quit
+"""
+
 class FileWindow(Frame):
     #initialize sub window as a frame
     def __init__(self,parent):
-        Frame.__init__(self, parent, bg="green")
+        Frame.__init__(self, parent, bd=5, relief=RIDGE)
         self.parent = parent
         self.widgets()
 
     #add widgets to window
     def widgets(self):
-        self.label = Label(self, text="File window")
-        self.label.grid(row=0, column=0, padx=DEFAULT_PAD, pady=DEFAULT_PAD, sticky="n")
-
         #displays path for selected folder
-        self.dir_path_label = ttk.Label(self, text="")
-        self.dir_path_label.grid(column=0, row=2, padx=DEFAULT_PAD, pady=DEFAULT_PAD)        
+        self.dir_path_label = ttk.Label(self, text="", relief=SUNKEN)
+        self.dir_path_label.grid(column=1, row=0, padx=DEFAULT_PAD, pady=DEFAULT_PAD)        
 
         #button which prompts for file selection
         self.browse_button = ttk.Button(self, text="Browse Files", command=lambda: browse_files(self.dir_path_label))
-        self.browse_button.grid(column=0, row=1, padx=DEFAULT_PAD, pady=DEFAULT_PAD)
+        self.browse_button.grid(column=0, row=0, padx=DEFAULT_PAD, pady=DEFAULT_PAD)
 
 class InfoWindow(Frame):
     #initialize sub window as a frame
     def __init__(self,parent):
-        Frame.__init__(self, parent, bg="purple")
+        Frame.__init__(self, parent, bd=5, relief=RIDGE, bg="purple")
         self.parent = parent
         self.widgets()
 
     #add widgets to window
     def widgets(self):
-        self.label = Label(self, text="Info window")
-        self.label.grid(row=0, column=0, padx=DEFAULT_PAD, pady=DEFAULT_PAD, sticky="n")
+        self.text = Label(self, text=info_text, justify=LEFT, relief=RIDGE, borderwidth=5, padx=DEFAULT_PAD, pady=DEFAULT_PAD)
+        self.text.grid(row=1, column=0, padx=DEFAULT_PAD, pady=DEFAULT_PAD)
 
 class ControlWindow(Frame):
     #initialize sub window as a frame
     def __init__(self,parent):
-        Frame.__init__(self, parent, bg="red")
+        Frame.__init__(self, parent, bd=5, relief=RIDGE, bg="red")
         self.parent = parent
         self.widgets()
 
     #add widgets to window
     def widgets(self):
-        self.label = Label(self, text="Control window")
-        self.label.grid(row=0, column=1, columnspan=2, padx=DEFAULT_PAD, pady=DEFAULT_PAD)
-        
         self.label2 = Label(self, text="Change Slide Duration")
         self.label2.grid(row=0, column=0, padx=DEFAULT_PAD, pady=DEFAULT_PAD, sticky="n")
 
         #send files to the pi
         #only appears when process_path determines a path is valid
-        self.send_button = ttk.Button(self, text="SEND", state=DISABLED, command=update_slides)
-        self.send_button.grid(row=1, column=2, padx=DEFAULT_PAD, pady=DEFAULT_PAD)
+        #self.send_button = ttk.Button(self, text="SEND", state=DISABLED, command=update_slides)
+        #self.send_button.grid(row=1, column=2, padx=DEFAULT_PAD, pady=DEFAULT_PAD)
+        
         #quit button
         self.quit = ttk.Button(self, text="Quit", command=self.parent.destroy)
         self.quit.grid(row=1, column=3, padx=DEFAULT_PAD, pady=DEFAULT_PAD)
@@ -71,19 +75,22 @@ class MainWindow(Tk):
     #init this window as root window
     def __init__(self):
         Tk.__init__(self)
+        ttk.Style().theme_use('default')
         self.mainWidgets()
         self.selectedFile = ""
 
     #add child windows to main window
     def mainWidgets(self):
+        self.info_window = InfoWindow(self)
+        self.info_window.grid(column=0, row=0, padx=DEFAULT_PAD, pady=DEFAULT_PAD, sticky="w")
+        
         self.file_window = FileWindow(self)
-        self.file_window.grid(column=0, row=0, padx=DEFAULT_PAD, pady=DEFAULT_PAD, sticky="w")
+        self.file_window.grid(column=0, row=1, padx=DEFAULT_PAD, pady=DEFAULT_PAD, sticky="w")
 
         self.control_window = ControlWindow(self)
         self.control_window.grid(column=0, row=2, columnspan=3, padx=DEFAULT_PAD, pady=DEFAULT_PAD, sticky="s")
 
-        self.info_window = InfoWindow(self)
-        self.info_window.grid(column=1, row=0, padx=DEFAULT_PAD, pady=DEFAULT_PAD, sticky="w")
+        
 
 #-----------------Functions--------------------------------------
 
